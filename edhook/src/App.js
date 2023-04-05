7291d5d97670c2271d473df4fcfd55b206067350
Valeria Masaltseva <valeriua.m@yandex.ru>
2023-04-05 15:08:15 +0300

    feat: add components and routing

--- edhook/src/App.js
+++ edhook/src/App.js
@@ -1,5 +1,15 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Registration } from "./Components/Registration/Registration";
+import { MainPage } from "./Components/MainPage/MainPage";
 
-export const App = () => {
-  return <div className="App"></div>;
-};
+export const App = () => (
+  <div className="App">
+    <BrowserRouter>
+      <Routes>
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </BrowserRouter>
+  </div>
+);
