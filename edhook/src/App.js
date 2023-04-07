0c060eaccbfdae9636c29e01dce1f7ce427184f9
Valeria Masaltseva <valeriua.m@yandex.ru>
2023-04-07 20:17:32 +0300

    feat: add Profile

--- edhook/src/App.js
+++ edhook/src/App.js
@@ -1,23 +1,20 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 import { Registration } from "./Pages/Registration/Registration";
 import { WelcomePage } from "./Pages/WelcomePage/WelcomePage";
-import { MainPage } from "./Pages/MainPage/MainPage";
+import { Profile } from "./Pages/Profile/Profile";
 import { RolePage } from "./Pages/RolePage/RolePage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 export const App = () => {
-    document.onload = () => {
-        console.log("aaa");
-    };
     return (
         <div className="container">
             <HashRouter>
                 <Routes>
-                    <Route path="/welcome" element={<WelcomePage />} />
+                    <Route path="/" element={<WelcomePage />} />
                     <Route path="/roles" element={<RolePage />} />
                     <Route path="/registration" element={<Registration />} />
-                    <Route path="/" element={<MainPage />} />
+                    <Route path="/profile" element={<Profile />} />
                 </Routes>
             </HashRouter>
         </div>
