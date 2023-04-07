d2223898e6c55e382958231ba34558921634d8cd
Alexey Abramov <alex93178@gmail.com>
2023-04-07 16:17:24 +0300

    feat: add group page and avatar component

---
+++ edhook/src/Components/Avatar.jsx
@@ -0,0 +1,7 @@
+export const Avatar = (props) => {
+  return (
+    <div className="props.classname">
+      <img src={props.src} alt="avatar" />
+    </div>
+  );
+};
---
+++ edhook/src/Pages/GroupPage/GroupPage.jsx
@@ -0,0 +1,15 @@
+import { Avatar } from "../../Components/Avatar";
+
+export const GropuPage = () => {
+  return (
+    <div>
+      <Avatar />
+    </div>
+  );
+  //GroupInfo
+  //notifications
+  //create notification
+  //list psrtisipants
+  //calendar
+  //chat
+};
